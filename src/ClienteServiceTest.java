b5575f8aa42b009bf22a21d0cda6fb44c99b8210
Vitoria.Cerqueira <vitoria.cerqueira@zup.com.br>
2023-06-20 13:25:02 -0300

    Update testes

--- src/ClienteServiceTest.java
+++ src/ClienteServiceTest.java
@@ -1,3 +1,4 @@
+import exception.RecursoNaoEncontradoException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -12,50 +13,66 @@ import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 public class ClienteServiceTest {
     Cliente cliente = new Cliente();
-    Map <String, Cliente> clientes = new HashMap<>();
+    Map<String, Cliente> clientes = new HashMap<>();
     @InjectMocks
     ClienteService clienteService = new ClienteService();
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         cliente = new Cliente("Vitoria", "vitoria@gmail.com", "123456", clientes);
     }
 
     @Test
-    public void testarCadastroDoCliente(){
+    public void testarCadastroDoCliente() {
 //        assertTrue(teclado.hasNext());
         clienteService.cadastrarCliente(cliente);
     }
+
     @Test
-    public void testarEmailValido(){
+    public void testarEmailValido() {
         String email = "vitoria@gmail.com";
         cliente = new Cliente("Vitoria", email, "123456", clientes);
-        String validarEmail = clienteService.validarEmail(email,clientes);
+        String validarEmail = clienteService.validarEmail(email, clientes);
         assertEquals(email, validarEmail);
     }
+
     @Test
-    public void testarListaDeClienteVazia(){
+    public void testarListaDeClienteVazia() {
         clientes.put(cliente.getCpf(), cliente);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
         clienteService.listarClientesCadastrados(new HashMap<>());
-        assertEquals("Nenhum cliente cadastrado",outputStream.toString().trim());
+        assertEquals("Nenhum cliente cadastrado", outputStream.toString().trim());
     }
+
     @Test
-    public void testarListaDeClienteCadastrado(){
+    public void testarListaDeClienteCadastrado() {
         clientes.put(cliente.getCpf(), cliente);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
         clienteService.listarClientesCadastrados(clientes);
-        assertEquals(clientes.values().toString().trim().replaceAll(("[*'[]']"), ""),outputStream.toString().trim());
+        assertEquals(clientes.values().toString().trim().replaceAll(("(\\[)|(\\])"), ""), outputStream.toString().trim());
     }
 
+//    @Test
+//    public void testarBuscarClientePorCpfVazio() {
+//        clienteService.buscarClientePorCpf(new HashMap<>(), cliente.getCpf());
+//        assertThrows(RecursoNaoEncontradoException.class, () -> clienteService.buscarClientePorCpf(new HashMap<>(), cliente.getCpf()));
+//    }
 
+    @Test
+    public void testarBuscarClientePorCpfVazio() {
+        RecursoNaoEncontradoException exception = assertThrows(
+                RecursoNaoEncontradoException.class, () ->
+                        clienteService.buscarClientePorCpf(new HashMap<>(), cliente.getCpf()));
 
+        assertEquals("O cliente não está cadastrado",exception.getMessage());
+    }
 
 }
